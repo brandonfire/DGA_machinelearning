cd36ece62600fe01b2bdd34e1b12e8110752feaf
brandonfire <chengbinhu@gmail.com>
2019-11-25 21:20:37 -0500

    Update SoftmaxforDGA.py

--- SoftmaxforDGA.py
+++ SoftmaxforDGA.py
@@ -80,9 +80,9 @@ def weight_variable(shape):
     initial = truncated_normal(shape,stddev=0.1)
     return tf.Variable(initial)
     
-def bias_variable(shape):
-    
-    pass
+#def bias_variable(shape):
+#    
+#    pass
     
 # Parameters
 learning_rate = 0.01
